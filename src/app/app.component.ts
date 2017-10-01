6b5f8937aa16f7b9d8e7c66adfdfbb0fcc7bc389
Ladeiras <jtladeiras@gmail.com>
2017-10-01 23:17:34 +0100

    Added modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -8,11 +9,19 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
+  constructor(private platform: Platform, private modalController: ModalController) {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      
+      this.openModal();
     });
+  }
+
+  openModal() {
+    let modal = this.modalController.create('ModalPage');
+    modal.onDidDismiss(() => {
+      this.openModal();
+    });
+    modal.present();
   }
 }
