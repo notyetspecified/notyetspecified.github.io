b92bd6ea5bebfbb1b229a67c6182883fce91d23b
joao <jtladeiras@gmail.com>
2017-10-03 19:15:14 +0100

    Build

---
+++ cordova_plugins.js
@@ -0,0 +1,35 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/www/browser/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.1.6",
+    "cordova-plugin-whitelist": "1.3.2",
+    "ionic-plugin-keyboard": "2.2.1"
+}
+// BOTTOM OF METADATA
+});
